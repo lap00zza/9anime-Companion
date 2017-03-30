cc8632e8c099af32a170216d24a30c3c3cca2087
Lapoozza <jewelmahanta@gmail.com>
2017-03-30 13:16:13 +0530

    test (animeUtils): add test for isUrl function
    The isUrl is a crucial function. It makes sure that only perfectly valid url are
    added to the local storage and later verified before binding to DOM. This
    keeps are safe from shady urls like *javascript:alert("Hi!")* or something
    similar and makes sure we dont execute unwanted javascript. The tests
    include cases to verify all these scenarios.

--- src/test/spec/animeUtils.spec.js
+++ src/test/spec/animeUtils.spec.js
@@ -2,9 +2,10 @@
  * Created by Jewel Mahanta (@lap00zza) on 26-03-2017.
  */
 /*global describe, beforeEach, inject, module, expect, it, sinon*/
-describe("Events Page Tests", function () {
+describe("Tests for animeUtils", function () {
 
     var testUrl = "https://9anime.is/watch/ao-haru-ride.qk5n/vpz64";
+
     describe("extractIdFromUrl, using: " + testUrl, function () {
 
         it("should return animeId qk5n", function () {
@@ -15,6 +16,23 @@ describe("Events Page Tests", function () {
         it("should return episodeId vpz64", function () {
             var extracted = window.animeUtils.extractIdFromUrl(testUrl);
             expect(extracted.episodeId).toBe("vpz64");
+        });
+    });
+
+    describe("isUrl", function () {
+        it("should return false when url is: javascript:alert(1)", function () {
+            var result = window.animeUtils.helper.isUrl("javascript:alert(1)");
+            expect(result).not.toBe(true);
+        });
+
+        it("should return false when url is: shpp://#$%%*12121210....90qwqw", function () {
+            var result = window.animeUtils.helper.isUrl("javascript:alert(1)");
+            expect(result).not.toBe(true);
+        });
+
+        it("should return true when url is: " + testUrl, function () {
+            var result = window.animeUtils.helper.isUrl(testUrl);
+            expect(result).toBe(true);
         });
     });
     
