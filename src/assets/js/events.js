4e4622a10e46ec6dd8aa9a8ef440fb6075e2f04d
Lapoozza <jewelmahanta@gmail.com>
2017-03-30 13:03:11 +0530

    ref (events, background): change if-else tree to switch-case
    Changed the if-else tree to switch-case as it was getting too big and
    clumsy. Now its much easier to follow.

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -26,121 +26,125 @@
 // main interface. This module runs in the background.
 //noinspection JSCheckFunctionSignatures
 chrome.runtime.onMessage.addListener(
+    // TODO: https://github.com/gorhill/uBlock/blob/master/src/js/messaging.js#L55 | use switch case?
     function (request, sender, sendResponse) {
-        if (request.intent === "hello") {
-            sendResponse({result: "Background page is working properly."});
-        }
+        switch (request.intent) {
+            case "hello":
+                sendResponse({result: "Background page is working properly."});
+                break;
 
-        else if (request.intent === "open_9anime") {
-            chrome.tabs.create({'url': "https://9anime.to"});
-            sendResponse({result: "opened"});
-        }
-
-        else if (request.intent === "open_anime") {
-            if (window.animeUtils.helper.isUrl(request.anime_url)) {
-                chrome.tabs.create({'url': request.anime_url});
+            case "open_9anime":
+                chrome.tabs.create({'url': "https://9anime.to"});
                 sendResponse({result: "opened"});
-            }
-        }
-            
-        else if (request.intent === "extractIdFromUrl") {
-            if (window.animeUtils.helper.isUrl(request.anime_url)) {
-                sendResponse({
-                    result: "success",
-                    data: window.animeUtils.extractIdFromUrl(request.anime_url)
-                });
-            }
-        }
-            
-        //  This event handles opening links to Reddit Discussions
-        //  TODO: things like 2nd Seasons etc should be converted to S2 and added to other names
-        else if (request.intent === "openRedditDiscussion") {
-            var cleanedTitle = request.name.replace(" (TV)", "").replace(" (Sub)", "").replace(" (Dub)", "").trim();
-            var url = "https://www.reddit.com/r/anime/search?q=";
+                break;
 
-            if (request.name && !request.episode) {
-                var titleText = `title:"${cleanedTitle}"`;
-                if (request.alternateNames.length > 0) {
-                    request.alternateNames.forEach(function (name) {
-                        titleText += ` OR title:"${name}"`;
+            case "open_anime":
+                if (window.animeUtils.helper.isUrl(request.anime_url)) {
+                    chrome.tabs.create({'url': request.anime_url});
+                    sendResponse({result: "opened"});
+                }
+                break;
+
+            case "extractIdFromUrl":
+                if (window.animeUtils.helper.isUrl(request.anime_url)) {
+                    sendResponse({
+                        result: "success",
+                        data: window.animeUtils.extractIdFromUrl(request.anime_url)
                     });
                 }
+                break;
 
-                // Deciding whether to add (selftext:MyAnimelist OR selftext:MAL) or not
-                // as not all discussion threads have MAL links. For now I will not add.
-                var params = `subreddit:anime self:yes title:"[Spoilers]" title:"[Discussion]" (${titleText})`;
-                chrome.tabs.create({
-                    "url": encodeURI(url + params + "&sort=new")
-                });
-                sendResponse({
-                    result: "opened"
-                });
+            case "openRedditDiscussion":
+                var cleanedTitle = request.name.replace(" (TV)", "").replace(" (Sub)", "").replace(" (Dub)", "").trim();
+                var url = "https://www.reddit.com/r/anime/search?q=";
 
-            } else if (request.name && request.episode) {
-                var titleText1 = `title:"${cleanedTitle} Episode ${request.episode}"`;
-                if (request.alternateNames.length > 0) {
-                    request.alternateNames.forEach(function (name) {
-                        titleText1 += ` OR title:"${name} Episode ${request.episode}"`;
+                if (request.name && !request.episode) {
+                    var titleText = `title:"${cleanedTitle}"`;
+                    if (request.alternateNames.length > 0) {
+                        request.alternateNames.forEach(function (name) {
+                            titleText += ` OR title:"${name}"`;
+                        });
+                    }
+
+                    // Deciding whether to add (selftext:MyAnimelist OR selftext:MAL) or not
+                    // as not all discussion threads have MAL links. For now I will not add.
+                    var params = `subreddit:anime self:yes title:"[Spoilers]" title:"[Discussion]" (${titleText})`;
+                    chrome.tabs.create({
+                        "url": encodeURI(url + params + "&sort=new")
+                    });
+                    sendResponse({
+                        result: "opened"
+                    });
+
+                } else if (request.name && request.episode) {
+                    var titleText1 = `title:"${cleanedTitle} Episode ${request.episode}"`;
+                    if (request.alternateNames.length > 0) {
+                        request.alternateNames.forEach(function (name) {
+                            titleText1 += ` OR title:"${name} Episode ${request.episode}"`;
+                        });
+                    }
+
+                    // Deciding whether to add (selftext:MyAnimelist OR selftext:MAL) or not
+                    // as not all discussion threads have MAL links. For now I will not add.
+                    var params1 = `subreddit:anime self:yes title:"[Spoilers]" title:"[Discussion]" (${titleText1})`;
+                    chrome.tabs.create({
+                        "url": encodeURI(url + params1 + "&sort=new")
+                    });
+                    sendResponse({
+                        result: "opened"
+                    });
+
+                } else {
+                    sendResponse({
+                        result: "fail"
                     });
                 }
+                break;
 
-                // Deciding whether to add (selftext:MyAnimelist OR selftext:MAL) or not
-                // as not all discussion threads have MAL links. For now I will not add.
-                var params1 = `subreddit:anime self:yes title:"[Spoilers]" title:"[Discussion]" (${titleText1})`;
-                chrome.tabs.create({
-                    "url": encodeURI(url + params1 + "&sort=new")
-                });
-                sendResponse({
-                    result: "opened"
-                });
-
-            } else {
-                sendResponse({
-                    result: "fail"
-                });
-            }
-        }
-
-        else if (request.intent === "addPinnedAnime") {
-            // Validate the url before adding it to list
-            if (request.animeName && window.animeUtils.helper.isUrl(request.animeUrl)) {
-                // console.log(name, url);
-                var pinPromise = window.animeUtils.addToPinnedList(request.animeName, request.animeUrl);
-                pinPromise.then(function (status) {
-                    sendResponse({
-                        result: status
+            case "addPinnedAnime":
+                // Validate the url before adding it to list
+                if (request.animeName && window.animeUtils.helper.isUrl(request.animeUrl)) {
+                    // console.log(name, url);
+                    var pinPromise = window.animeUtils.addToPinnedList(request.animeName, request.animeUrl);
+                    pinPromise.then(function (status) {
+                        sendResponse({
+                            result: status
+                        });
                     });
-                });
 
-                // We return true to indicate we wish to send a response
-                // asynchronously (this will keep the message channel
-                // open to the other end until sendResponse is called)
-                return true;
+                    // We return true to indicate we wish to send a response
+                    // asynchronously (this will keep the message channel
+                    // open to the other end until sendResponse is called)
+                    return true;
 
-            } else {
-                sendResponse({
-                    result: "fail"
-                });
-            }
-        }
-
-        else if (request.intent === "removePinnedAnime") {
-            if (window.animeUtils.helper.isUrl(request.animeUrl)) {
-                var remPinPromise = window.animeUtils.removeFromPinnedList(request.animeUrl);
-
-                remPinPromise.then(function (status) {
+                } else {
                     sendResponse({
-                        result: status.result,
-                        itemCount: status.itemCount
+                        result: "fail"
                     });
-                });
+                }
+                break;
 
-                return true;
-            } else {
-                sendResponse({
-                    result: "fail"
-                });
-            }
+            case "removePinnedAnime":
+                if (window.animeUtils.helper.isUrl(request.animeUrl)) {
+                    var remPinPromise = window.animeUtils.removeFromPinnedList(request.animeUrl);
+
+                    remPinPromise.then(function (status) {
+                        sendResponse({
+                            result: status.result,
+                            itemCount: status.itemCount
+                        });
+                    });
+
+                    // We return true to indicate we wish to send a response
+                    // asynchronously (this will keep the message channel
+                    // open to the other end until sendResponse is called)
+                    return true;
+                } else {
+                    sendResponse({
+                        result: "fail"
+                    });
+                }
+                break;
         }
     }
 );
