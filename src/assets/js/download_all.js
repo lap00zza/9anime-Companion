77781deaa24cd92448689b2a55b0acb183a90cd7
Lapoozza <jewelmahanta@gmail.com>
2017-04-09 17:30:55 +0530

    ref (download_all): Changed license and added filename safe regex
    - Changed the license from MIT to The 3-Clause BSD
    - Added a function which generate filename safe strings from anime titles.

--- src/assets/js/download_all.js
+++ src/assets/js/download_all.js
@@ -1,29 +1,51 @@
 /**
- *  MIT License
+ * Copyright 2017 Jewel Mahanta(@lap00zza)
  *
- *  Copyright (c) 2017 Jewel Mahanta
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * 1. Redistributions of source code must retain the above copyright notice, this list
+ *    of conditions and the following disclaimer.
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
  */
+// NOTES:
+// 1. JQUERY is only used for the AJAX function. So, it is 100% possible to convert this
+//    to a pure javascript based library.
+// 2. The downloads start at 5 seconds interval. Please don't try to decrease this. If you
+//    do, your IP might get flagged for spamming.
 (function ($) {
 
     var downloadAll = window.downloadAll = downloadAll || {};
+
+    /**
+     * Just as the function name says!
+     * We replace the illegal characters with underscore (_)
+     *
+     * @param filename
+     * @returns {void|XML|string|*}
+     */
+    function generateFileSafeString(filename) {
+        var re = /[\\\/<>*?:"|]/gi;
+        return filename.replace(re, "_");
+    }
 
     /**
      * Fetch the grabber information. These are necessary to use the
@@ -59,7 +81,7 @@
     };
 
     /**
-     *  Get the list of downloadable files using the file grabber.
+     * Get the list of downloadable files using the file grabber.
      *
      * @param grabberUrl
      * @param episodeId
@@ -105,7 +127,7 @@
      *                          Whether we will use the chrome downloader or external downloader
      * @param {String} baseUrl - The current base url. Example: https://9anime.tv, https://9anime.is etc.
      */
-    downloadAll.downloadFiles = function (episodes, name, quality = "720p", method = "browser", baseUrl = "https://9anime.to") {
+    downloadAll.downloadFiles = function (episodes, name, quality = "360p", method = "browser", baseUrl = "https://9anime.to") {
         // TODO: do we really need a promise for this?
         // TODO: if we do use promise, maybe add a progress callback?
         return new Promise(function (resolve, reject) {
@@ -159,7 +181,7 @@
                                         if(fileQuality === quality) {
                                             chrome.downloads.download({
                                                 url: fileUrl,
-                                                filename: `9anime Companion/${name} - E${ep_number}.${fileType}`,
+                                                filename: `9anime Companion/${generateFileSafeString(name)} - E${ep_number}.${fileType}`,
                                                 conflictAction: "uniquify"
 
                                             }, function (downloadId) {
