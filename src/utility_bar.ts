3927bcb9b520f4114248a334f8ba23455685af51
Jewel Mahanta <jewelmahanta@gmail.com>
2017-07-09 14:26:16 +0530

    ref: changed from class to function

--- src/utility_bar.ts
+++ src/utility_bar.ts
@@ -1,32 +1,29 @@
 import RedditDiscussion from "./reddit_discussion";
 import * as utils from "./utils";
 
-export default class UtilityBar {
-    private redditSearchUrl: string;
-    private malSearchUrl: string;
-    private redditLogo: string = chrome.extension.getURL("assets/images/reddit-icon.png");
-    private malLogo: string = chrome.extension.getURL("assets/images/mal-icon.png");
+let redditLogo = chrome.extension.getURL("assets/images/reddit-icon.png");
+let malLogo = chrome.extension.getURL("assets/images/mal-icon.png");
 
-    public constructor(name: string, altNames: string[], episode: string = "") {
-        this.redditSearchUrl = new RedditDiscussion(name, altNames, episode).url();
-        this.malSearchUrl = "https://myanimelist.net/anime.php?q=" + name;
-    }
+/**
+ * Returns a string template of the Utility Bar.
+ * @param {string} name
+ * @param {string} episode
+ * @param {string[]} altNames
+ * @returns {string} Utility Bar
+ */
+export function template(name: string, episode: string = "", altNames: string[] = []): string {
+    let redditSearchUrl = new RedditDiscussion(name, episode, altNames).url();
+    let malSearchUrl = "https://myanimelist.net/anime.php?q=" + name;
 
-    public generate(): string {
-        return this.template();
-    }
-
-    private template(): string {
-        return utils.dedent(
-            `<div id="nac__utility-bar">
-                <a class="utility" href="${this.redditSearchUrl}" target="_blank">
-                    <img src='${this.redditLogo}'>
-                    Reddit Discussion
-                </a>
-                <a class="utility" href="${this.malSearchUrl}" target="_blank">
-                    <img src='${this.malLogo}'>
-                    Find in MAL
-                </a>
-            </div>`);
-    }
+    return utils.dedent(
+        `<div id="nac__utility-bar">
+            <a class="utility" href="${redditSearchUrl}" target="_blank">
+                <img src='${redditLogo}'>
+                Reddit Discussion
+            </a>
+            <a class="utility" href="${malSearchUrl}" target="_blank">
+                <img src='${malLogo}'>
+                Find in MAL
+            </a>
+        </div>`);
 }
