bf65dafc9579f3ad12be2a3d05b09a5a11d802e0
Jewel Mahanta <jewelmahanta@gmail.com>
2017-07-17 12:52:45 +0530

    test (utils): tests for most of the functions in util
    PR's are welcome to help improve the tests.

---
+++ test/utils.test.js
@@ -0,0 +1,47 @@
+/* global test, expect */
+const utils = require("../src/utils")
+
+// [1]: fileSafeString
+test("fileSafeString: illegal characters should be removed", () => {
+    expect(utils.fileSafeString("nar::to")).toBe("narto")
+    expect(utils.fileSafeString("nar\\\/to")).toBe("narto")
+    expect(utils.fileSafeString("nar<>?*|'to")).toBe("nar'to")
+});
+
+// [2]: pad
+test("pad: should be padded if less than 3 digit", () => {
+    expect(() => {
+        utils.pad("")
+    }).toThrowError("num can't be a blank string")
+    expect(utils.pad("1")).toBe("001");
+    expect(utils.pad("11")).toBe("011");
+})
+
+test("pad: should not be padded if 3 or more digit", () => {
+    expect(utils.pad("111")).toBe("111");
+    expect(utils.pad("1111")).toBe("1111");
+})
+
+// [3]: mergeObject
+test("mergeObject: should merge given 2 objects", () => {
+    let obj1 = {a: 1, b: 2}
+    let obj2 = {c: 3, d: 4}
+    expect(utils.mergeObject(obj1, obj2)).toEqual({a: 1, b: 2, c: 3, d: 4})
+})
+
+// [4]: decomposeURL
+test("decomposeURL: should decompose given a proper uri", () => {
+    let uri = "https://9anime.to/grabber-api/?server=21";
+    expect(utils.decomposeURL(uri)).toEqual(["https://9anime.to/grabber-api/", {server: "21"}])
+
+    let uri1 = "https://9anime.to/grabber-api/?server=21&a=b";
+    expect(utils.decomposeURL(uri1)).toEqual(["https://9anime.to/grabber-api/", {server: "21", a: "b"}])
+})
+
+test("decomposeURL: should ignore params that are not proper", () => {
+    let uri = "https://9anime.to/grabber-api/?server=21&hello";
+    expect(utils.decomposeURL(uri)).toEqual(["https://9anime.to/grabber-api/", {server: "21"}])
+
+    let uri1 = "https://9anime.to/grabber-api/?server=21&";
+    expect(utils.decomposeURL(uri1)).toEqual(["https://9anime.to/grabber-api/", {server: "21"}])
+})
