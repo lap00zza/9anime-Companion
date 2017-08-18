a4dcf1e4ac92407cb25f0a5ce998767d075bea63
Jewel Mahanta <jewelmahanta@gmail.com>
2017-08-18 18:39:44 +0530

    ref (manifest): removed options_ui html/js in favour of open_in_tab

--- platform/chromium/options_ui.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    // 0 means Intent.Open_Options. Check
-    // src/ts/common.ts for more details
-    chrome.runtime.sendMessage({
-        intent: 0,
-    });
-    window.close();
-})();
