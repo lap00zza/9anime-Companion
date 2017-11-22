c87816c5604b825b86cfeeda73a44f1151c9a14f
Jewel Mahanta <jewelmahanta@gmail.com>
2017-11-23 03:13:16 +0530

    (ref #101): inject disableAdblockDetection regardless of setting
    Now folks should be able to enable/disable remove ads and the extension
    will work as expected. @dexatq also suggested an alternate option, which
    is to disable remove ads and disable social share.

--- src/content_script/global_pre.ts
+++ src/content_script/global_pre.ts
@@ -11,29 +11,57 @@ import {loadSettings} from "../lib/utils";
 
 console.info("9anime Companion 1.0.0 (Global Script Pre)");
 
-loadSettings([
-    "remAds",
-]).then(settings => {
-    /**
-     * The following is mainly to counter this specific script that 9anime uses.
-     * According to my personal research, this accounts for most of the popups.
-     *
-     *      <script src="//go.oclasrv.com/apu.php?zoneid=1308556" data-cfasync="false"
-     *      async="" onerror="_elavfx()" onload="_msdurx()"></script>
-     *
-     * The following adds the _elavfx and _msdurx methods to window and makes sure
-     * they cant be overwritten by 9anime. Thus, stopping (most of) the popups.
-     */
-    // 20-11-2017
-    // 9anime renamed _elavfx and _msdurx to _fsbfifi and _qcvrgvy
-    // 22-11-2017
-    // 9anime added fai to thwart adblock :)
-    if (settings.remAds) {
-        console.info("9anime Companion: Neutralizing ads");
-        let head = document.querySelectorAll("head");
-        let script = document.createElement("script");
-        script.innerText =
-            `window.fai = {
+/* --- Disabling the adblock detection --- */
+// 22-11-2017
+// 9anime added fai to thwart adblock :)
+const disableAdblockDetection = () => {
+    console.info("9anime Companion: disabling adblock detection");
+    let head = document.querySelectorAll("head");
+    let script = document.createElement("script");
+    script.innerText =
+        `window.fai = {
+                M: {},
+                a: {}
+            };
+            Object.defineProperty(window, "fai", {
+                enumerable: true,
+                configurable: false,
+                writable: false
+            });`.replace(/\s{2,}/g, "");
+    // Script inject is required because thats the only way to
+    // manipulate the global window object.
+    head[0].appendChild(script);
+};
+/* --- ~~~ --- */
+
+// schedule to run the following on the nextTick.
+setTimeout(() => {
+    // Always do this. Why? Because 9anime outright blocks
+    // 9anime Companion of this is not done.
+    disableAdblockDetection();
+
+    // Do only if settings allow
+    loadSettings([
+        "remAds",
+    ]).then(settings => {
+        /**
+         * The following is mainly to counter this specific script that 9anime uses.
+         * According to my personal research, this accounts for most of the popups.
+         *
+         *      <script src="//go.oclasrv.com/apu.php?zoneid=1308556" data-cfasync="false"
+         *      async="" onerror="_elavfx()" onload="_msdurx()"></script>
+         *
+         * The following adds the _elavfx and _msdurx methods to window and makes sure
+         * they cant be overwritten by 9anime. Thus, stopping (most of) the popups.
+         */
+        // 20-11-2017
+        // 9anime renamed _elavfx and _msdurx to _fsbfifi and _qcvrgvy
+        if (settings.remAds) {
+            console.info("9anime Companion: Neutralizing ads");
+            let head = document.querySelectorAll("head");
+            let script = document.createElement("script");
+            script.innerText =
+                `window.fai = {
                 M: {},
                 a: {}
             };
@@ -58,8 +86,9 @@ loadSettings([
                 configurable: false,
                 writable: false
             });`.replace(/\s{2,}/g, "");
-        // Script inject is required because thats the only way to
-        // manipulate the global window object.
-        head[0].appendChild(script);
-    }
+            // Script inject is required because thats the only way to
+            // manipulate the global window object.
+            head[0].appendChild(script);
+        }
+    });
 });
